ceb30b6098d916742d419a05b463a7efe0d43fc9
Ollie <oliverjhall@hotmail.co.uk>
2024-01-11 22:12:07 +0000

    Hardcoded local skeleton of verifying email address

---
+++ src/pages/EmailConfirmation.tsx
@@ -0,0 +1,50 @@
+import { Typography, styled } from "@mui/material";
+import { useState, useEffect} from "react"
+
+export const EmailConfirmationPage = () => {
+    const [text, setText] = useState("Verifying your email address")
+    const params = new URLSearchParams(window.location.search)
+    let emailAddress = params.get("email")
+    let verificationCode = params.get("verification")
+
+    useEffect(() => {
+        if (emailAddress === null || verificationCode === null) return;
+        console.log(`https://localhost:7225/api/verify-email?emailAddress=${emailAddress}&verificationCode=${verificationCode}`)
+        fetch(`https://localhost:7225/api/verify-email?emailAddress=${emailAddress}&verificationCode=${verificationCode}`)
+          .then((res) => {
+            if (res.status === 200) {
+                setText("Thank you - your email address has been verified");
+            }
+            if (res.status === 409) {
+                setText("Your email address has already been verified");
+            }
+          })
+          .catch(error => {
+            setText("Email cannot be verified. Please try again or email info@stridewars.com if you are having issues");
+            console.log("error", error)
+          })
+      }, []);
+
+    return (
+        <>
+            <header>
+                <Typography variant="h1">
+                    Verifiying Email
+                </Typography>
+                <Typography>
+                    {text}
+                </Typography>
+            </header>
+            <FirstCompetitionSection>
+            </FirstCompetitionSection>
+        </>
+    );
+}
+
+const FirstCompetitionSection = styled(`section`)({
+    backgroundColor: "#111",
+    color: "white",
+    padding: "30px 20px",
+    boxShadow: "0 4px 6px rgba(35, 35, 35, 0.15)",
+    textAlign: "center"
+});
--- src/routing/BaseRouter.tsx
+++ src/routing/BaseRouter.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import { App } from "../App";
 import { Template } from "./Template";
 import { LandingPage } from "../pages/LandingPage";
+import { EmailConfirmationPage } from "../pages/EmailConfirmation";
 
 export const BaseRouter = () => 
     <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="confirmation" element={<EmailConfirmationPage />} />
         <Route path="dashboard">
             <Route element={<Template />}>
                 <Route index element={<App/>} />
